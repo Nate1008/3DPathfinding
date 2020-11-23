573b25cd81e2cbc39a44955e76c4aba0232a3efa
Nate1008 <pidgeypiggy1000@gmail.com>
2020-11-23 15:13:41 -0500

    Updating...

--- src/algos.js
+++ src/algos.js
@@ -16,6 +16,7 @@ const dfs = (board, boardCoor, startCoor, targetCoor, diagonal, rows) => {
           console.log('TARGET WAS FOUND');
           return;
         }
+
         if (boardCoor[node[0]][node[1]] === 0) {
             boardCoor[node[0]][node[1]] = 2;
             let dirs = [];
@@ -43,16 +44,34 @@ const dfs = (board, boardCoor, startCoor, targetCoor, diagonal, rows) => {
 }
 
 const bfs = (board, boardCoor, startCoor, targetCoor, diagonal, rows) => {
-    visited[node] = true;
-    queue.equeue(node);
-    while (!queue.isEmpty()) {
-        let node = queue.dequeue();
-        console.log(`we visited ${visiting}`)
-        for (let j = 0; j < graphAdj[visiting].length; j++) {
-            if ((graphAdj[visiting][j] === 1) && (visited[j] === false)) {
-              visited[j] = true;
-              queue.equeue(j);
+    boardCoor[startCoor[0]][startCoor[1]] = 2;
+    let queue = [];
+    queue.unshift(startCoor);
+    while (queue.length) {
+        let node = queue.pop();
+        if (node[0] == targetCoor[0] && node[1] == targetCoor[1]) {
+          console.log('TARGET WAS FOUND');
+          return;
+        }
+
+        let dirs = [];
+        if (diagonal) {
+          dirs = eightdir;
+        } else {
+          dirs = fourdir;
+        }
+        for(let dir of dirs) {
+            let neighbor;
+            try {
+                neighbor = boardCoor[node[0] + dir[0]][node[1] + dir[1]];
+            } catch (err) {
+              continue;
             }
+            // board[(node[0] + dir[0] * rows * 2) + (node[1] + dir[1])].material = THREE.MeshBasicMaterial({ color: 0x00b8d2 })
+        }
+        if (neighbor === 0) {
+          boardCoor[node[0]][node[1]] === 0
+          queue.unshift([ (node[0] + dir[0]), (node[1] + dir[1]) ]);
         }
     }
 }
@@ -61,4 +80,4 @@ const bfs = (board, boardCoor, startCoor, targetCoor, diagonal, rows) => {
 
 
 
-export { dfs };
+export { dfs, bfs };
