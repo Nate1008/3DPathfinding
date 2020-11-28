6350323629c15b56429da2498941942230e6cd64
Nate1008 <pidgeypiggy1000@gmail.com>
2020-11-28 10:20:22 -0500

    DFS Visualizer Visualizes

--- src/algos.js
+++ src/algos.js
@@ -1,87 +1,126 @@
-
 import * as THREE from 'three';
 
-const fourdir = [[0, 1], [1, 0], [-1, 0], [0, -1]];
-const eightdir = [[0, 1], [-1, 1], [1, 0], [1, -1], [-1, 0], [-1, 1], [0, -1], [1, -1]];
+const fourdir = [
+    [0, 1],
+    [1, 0],
+    [-1, 0],
+    [0, -1]
+];
+const eightdir = [
+    [0, 1],
+    [-1, 1],
+    [1, 0],
+    [1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [1, -1]
+];
+let queue = [];
 
-
-const dfs = (board, boardCoor, boardPath, startCoor, targetCoor, diagonal, rows) => {
-    let stack = [];
-    stack.push(startCoor);
+const reset = (boardPath, startCoor) => {
+    queue = [startCoor];
     boardPath[startCoor[0]][startCoor[1]].push(startCoor);
-    while(stack.length) {
-        let node = stack.pop();
-        if (node[0] == targetCoor[0] && node[1] == targetCoor[1]) {
-          console.log('TARGET WAS FOUND');
-          console.log(boardPath[node[0]][node[1]]);
-          return boardPath[node[0]][node[1]];
-        }
-
-        if (boardCoor[node[0]][node[1]] === 0) {
-            boardCoor[node[0]][node[1]] = 2;
-            let dirs = [];
-            if (diagonal) {
-              dirs = eightdir;
-            } else {
-              dirs = fourdir;
-            }
-            for(let dir of dirs) {
-                let neighbor;
-                try {
-                    neighbor = boardCoor[node[0] + dir[0]][node[1] + dir[1]];
-                } catch (err) {
-                  continue;
-                }
-                // board[(node[0] + dir[0] * rows * 2) + (node[1] + dir[1])].material = new THREE.MeshBasicMaterial({ color: 0x00b8d2 });
-                if (neighbor === 0) {
-                  let nextCoor = [ (node[0] + dir[0]), (node[1] + dir[1]) ]
-                  stack.push(nextCoor);
-                  boardPath[nextCoor[0]][nextCoor[1]] = boardPath[node[0]][node[1]].concat([nextCoor]);
-                }
-            }
-        }
-    }
-    console.log('TARGET WAS NOT FOUND');
-    return;
 }
 
-const bfs = (board, boardCoor, boardPath, startCoor, targetCoor, diagonal, rows) => {
-    let queue = [];
-    queue.unshift(startCoor);
-    boardPath[startCoor[0]][startCoor[1]].push(startCoor);
-    boardCoor[startCoor[0]][startCoor[1]] = 2;
-    while (queue.length) {
-        let node = queue.pop()
-        console.log(node);
-        if (node[0] == targetCoor[0] && node[1] == targetCoor[1]) {
-          console.log('TARGET WAS FOUND');
-          console.log(boardPath[node[0]][node[1]]);
-          return boardPath[node[0]][node[1]];
-        }
+const dfs = (board, boardCoor, boardPath, node, startCoor, targetCoor, diagonal, rows) => {
+    console.log(node);
+    if (node[0] == targetCoor[0] && node[1] == targetCoor[1]) {
+        console.log('TARGET WAS FOUND');
+        console.log(boardPath[node[0]][node[1]]);
+        return boardPath[node[0]][node[1]];
+    }
 
-        if (!(node[0] === startCoor[0] && node[1] == startCoor[1])) {
-            board[(rows * node[1]) + node[0]].material = new THREE.MeshBasicMaterial({ color: 0x00b8d2 });
-        }
+    if (!(node[0] === startCoor[0] && node[1] == startCoor[1])) {
+        board[(rows * node[1]) + node[0]].material = new THREE.MeshBasicMaterial({
+            color: 0x00b8d2
+        });
+    }
+
+    if (boardCoor[node[0]][node[1]] === 0) {
+        boardCoor[node[0]][node[1]] = 2;
         let dirs = [];
         if (diagonal) {
-          dirs = eightdir;
+            dirs = eightdir;
         } else {
-          dirs = fourdir;
+            dirs = fourdir;
         }
-        for(let dir of dirs) {
+        let ret;
+        for (let dir of dirs) {
             let neighbor;
             try {
                 neighbor = boardCoor[node[0] + dir[0]][node[1] + dir[1]];
             } catch (err) {
-              continue;
+                continue;
             }
             if (neighbor === 0) {
-              neighbor = 2;
-              let nextCoor = [ (node[0] + dir[0]), (node[1] + dir[1]) ]
-              boardPath[nextCoor[0]][nextCoor[1]] = boardPath[node[0]][node[1]].concat([nextCoor]);
-              queue.unshift(nextCoor);
+                let nextCoor = [(node[0] + dir[0]), (node[1] + dir[1])];
+                boardPath[nextCoor[0]][nextCoor[1]] = boardPath[node[0]][node[1]].concat([nextCoor]);
+                ret = setTimeout(() => {
+                    let _ret = dfs(board, boardCoor, boardPath, nextCoor, startCoor, targetCoor, diagonal, rows);
+                    if (_ret) {
+                        return _ret;
+                    }
+                }, 500);
+                return ret;
+
+
             }
         }
+    }
+    console.log('TARGET WAS NOT FOUND');
+    return;
+}
+
+
+const bfs = (board, boardCoor, boardPath, startCoor, targetCoor, diagonal, rows) => {
+    if (!queue.length) {
+        console.log('TARGET WAS NOT FOUND');
+        return;
+    }
+    let node = queue.pop();
+
+    if (boardCoor[node[0]][node[1]] === 0) {
+        boardCoor[node[0]][node[1]] = 2;
+        if (node[0] == targetCoor[0] && node[1] == targetCoor[1]) {
+            console.log('TARGET WAS FOUND');
+            console.log(boardPath[node[0]][node[1]]);
+            return boardPath[node[0]][node[1]];
+        }
+
+        if (!(node[0] === startCoor[0] && node[1] == startCoor[1])) {
+            board[(rows * node[1]) + node[0]].material = new THREE.MeshBasicMaterial({
+                color: 0x00b8d2
+            });
+        }
+        let dirs = [];
+        if (diagonal) {
+            dirs = eightdir;
+        } else {
+            dirs = fourdir;
+        }
+
+        let ret;
+        for (let dir of dirs) {
+            let neighbor;
+            try {
+                neighbor = boardCoor[node[0] + dir[0]][node[1] + dir[1]];
+            } catch (err) {
+                continue;
+            }
+            if (neighbor === 0) {
+                let nextCoor = [(node[0] + dir[0]), (node[1] + dir[1])]
+                boardPath[nextCoor[0]][nextCoor[1]] = boardPath[node[0]][node[1]].concat([nextCoor]);
+                queue.unshift(nextCoor);
+            }
+        }
+        ret = setTimeout(() => {
+            let _ret = bfs(board, boardCoor, boardPath, startCoor, targetCoor, diagonal, rows);
+            if (_ret) {
+                return _ret;
+            }
+        }, 500);
+        return ret;
     }
     console.log('TARGET WAS NOT FOUND');
     return;
@@ -91,4 +130,8 @@ const bfs = (board, boardCoor, boardPath, startCoor, targetCoor, diagonal, rows)
 
 
 
-export { dfs, bfs };
+export {
+    reset,
+    dfs,
+    bfs
+};
--- src/index.js
+++ src/index.js
@@ -8,13 +8,18 @@
 
 import * as THREE from 'three';
 import Stats from 'three/examples/jsm/libs/stats.module.d.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import {
+    OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
-import { dfs, bfs } from './algos.js';
+import {
+    reset,
+    dfs,
+    bfs
+} from './algos.js';
 
 
 //3D Pathfinding
-
 
 //LETS
 let group = new THREE.Group();
@@ -35,66 +40,78 @@ const height = width / 2;
 
 const ground = new THREE.Mesh(
     new THREE.BoxGeometry(width, height, width),
-    new THREE.MeshBasicMaterial({ color: 0xe1e1e1 })
+    new THREE.MeshBasicMaterial({
+        color: 0xe1e1e1
+    })
 );
 
 const build = new THREE.Mesh(
     new THREE.BoxGeometry(width, height + 1, width),
-    new THREE.MeshBasicMaterial({ color: 0x6f6c6c})
+    new THREE.MeshBasicMaterial({
+        color: 0x6f6c6c
+    })
 );
 
 const start = new THREE.Mesh(
     new THREE.BoxGeometry(width, height + 1, width),
-    new THREE.MeshBasicMaterial({ color: 0x4fc134 })
+    new THREE.MeshBasicMaterial({
+        color: 0x4fc134
+    })
 );
 
 const target = new THREE.Mesh(
     new THREE.BoxGeometry(width, height + 1, width),
-    new THREE.MeshBasicMaterial({ color: 0xff2020 })
+    new THREE.MeshBasicMaterial({
+        color: 0xff2020
+    })
 );
 
 const weight = new THREE.Mesh(
     new THREE.BoxGeometry(width, height + 1, width),
-    new THREE.MeshBasicMaterial({ color: 0x02abed })
+    new THREE.MeshBasicMaterial({
+        color: 0x02abed
+    })
 );
 
 const getBoard = () => {
     group.traverse((node) => {
-      if (!(node instanceof THREE.Mesh)) {
-        return;
-      } else if (node.material.side === THREE.BackSide) {
-        return;
-      }
-      let r = Math.round(rows + (node.position.x / (width + 0.05)));
-      let c = Math.round(cols + (node.position.z / (width + 0.05)));
-      boardPath[c][r] = [];
-      switch(board[((2 * rows) * r) + c].material.color.getHex()){
-          case start.material.color.getHex():
-                  startCoor = [c, r];
-                  boardCoor[c][r] = 0;
-                  break;
-          case target.material.color.getHex():
-                  targetCoor = [c, r];
-                  boardCoor[c][r] = 0;
-                  break;
-          case build.material.color.getHex():
-                  boardCoor[c][r] = 1;
-                  break;
-          default:
-                  boardCoor[c][r] = 0;
-      }
+        if (!(node instanceof THREE.Mesh)) {
+            return;
+        } else if (node.material.side === THREE.BackSide) {
+            return;
+        }
+        let r = Math.round(rows + (node.position.x / (width + 0.05)));
+        let c = Math.round(cols + (node.position.z / (width + 0.05)));
+        boardPath[c][r] = [];
+        switch (board[((2 * rows) * r) + c].material.color.getHex()) {
+            case start.material.color.getHex():
+                startCoor = [c, r];
+                boardCoor[c][r] = 0;
+                break;
+            case target.material.color.getHex():
+                targetCoor = [c, r];
+                boardCoor[c][r] = 0;
+                break;
+            case build.material.color.getHex():
+                boardCoor[c][r] = 1;
+                break;
+            default:
+                boardCoor[c][r] = 0;
+        }
     })
 }
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 30, 0);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
 renderer.setClearColor(0xffffff, 1);
-renderer.setSize( window.innerWidth + 10000, window.innerHeight + 10000);
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth + 10000, window.innerHeight + 10000);
+document.body.appendChild(renderer.domElement);
 
 const stats = Stats();
 document.body.appendChild(stats.dom);
@@ -109,12 +126,17 @@ for (let r = -rows; r < rows; r++) {
 
         let cube = new THREE.Mesh(
             new THREE.BoxGeometry(width, height, width),
-            new THREE.MeshBasicMaterial({ color: 0xe1e1e1 })
+            new THREE.MeshBasicMaterial({
+                color: 0xe1e1e1
+            })
         );
 
         let outline = new THREE.Mesh(
             new THREE.BoxGeometry(width, height, width),
-            new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.BackSide})
+            new THREE.MeshBasicMaterial({
+                color: 0x000000,
+                side: THREE.BackSide
+            })
         );
 
         cube.position.set(r * (width + 0.05), 0, c * (width + 0.05));
@@ -135,7 +157,7 @@ scene.add(group);
 
 
 
-let controls = new OrbitControls( camera, renderer.domElement );
+let controls = new OrbitControls(camera, renderer.domElement);
 controls.enablePan = false;
 controls.enableRotate = true;
 controls.enableDamping = true;
@@ -157,8 +179,8 @@ const toggleNode = () => {
     let cs = document.getElementsByClassName("c")[0];
     let labels = ["Start", "Target", "Wall", "Weighted"];
     let counter = 0;
-    for(let i = 0; i < 4; i++) {
-        if (labels[i] + " Node" == cs.textContent){
+    for (let i = 0; i < 4; i++) {
+        if (labels[i] + " Node" == cs.textContent) {
             counter = i;
             counter++;
             break;
@@ -168,25 +190,25 @@ const toggleNode = () => {
 }
 
 const clearAll = () => {
-    for(let i = 0; i < board.length; i++) {
-       board[i].scale.y = 1;
-       board[i].material = ground.material;
-       board[i].position.y = 0;
-       outlines[i].scale.y = 1.05;
-       outlines[i].position.y = 0;
+    for (let i = 0; i < board.length; i++) {
+        board[i].scale.y = 1;
+        board[i].material = ground.material;
+        board[i].position.y = 0;
+        outlines[i].scale.y = 1.05;
+        outlines[i].position.y = 0;
     }
     let cs = document.getElementsByClassName("c")[0];
     cs.textContent = "Start Node";
 }
 
 const clearType = (type) => {
-    for(let i = 0; i < board.length; i++) {
+    for (let i = 0; i < board.length; i++) {
         if (board[i].material.color.getHex() === type.material.color.getHex()) {
-           board[i].scale.y = 1;
-           board[i].material = ground.material;
-           board[i].position.y = 0;
-           outlines[i].scale.y = 1.05;
-           outlines[i].position.y = 0;
+            board[i].scale.y = 1;
+            board[i].material = ground.material;
+            board[i].position.y = 0;
+            outlines[i].scale.y = 1.05;
+            outlines[i].position.y = 0;
         }
     }
 }
@@ -199,48 +221,53 @@ const clearWall = () => {
 }
 
 const resizeBoard = (newRows) => {
-  boardCoor = [];
-  outlines = [];
-  board = [];
-  boardPath = [];
-  let selectedObject = scene.getObjectByName(group.name);
-  scene.remove( selectedObject );
-  let newGroup = new THREE.Group();
-  for (let r = -newRows; r < newRows; r++) {
-      boardCoor.push([]);
-      boardPath.push([]);
-      for (let c = -newRows; c < newRows; c++) {
-          boardCoor[r + newRows].push(0);
-          boardPath[r + newRows].push([]);
-          let shape = new THREE.Group();
+    boardCoor = [];
+    outlines = [];
+    board = [];
+    boardPath = [];
+    let selectedObject = scene.getObjectByName(group.name);
+    scene.remove(selectedObject);
+    let newGroup = new THREE.Group();
+    for (let r = -newRows; r < newRows; r++) {
+        boardCoor.push([]);
+        boardPath.push([]);
+        for (let c = -newRows; c < newRows; c++) {
+            boardCoor[r + newRows].push(0);
+            boardPath[r + newRows].push([]);
+            let shape = new THREE.Group();
 
-          let cube = new THREE.Mesh(
-              new THREE.BoxGeometry(width, height, width),
-              new THREE.MeshBasicMaterial({ color: 0xe1e1e1 })
-          );
+            let cube = new THREE.Mesh(
+                new THREE.BoxGeometry(width, height, width),
+                new THREE.MeshBasicMaterial({
+                    color: 0xe1e1e1
+                })
+            );
 
-          let outline = new THREE.Mesh(
-              new THREE.BoxGeometry(width, height, width),
-              new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.BackSide})
-          );
+            let outline = new THREE.Mesh(
+                new THREE.BoxGeometry(width, height, width),
+                new THREE.MeshBasicMaterial({
+                    color: 0x000000,
+                    side: THREE.BackSide
+                })
+            );
 
-          cube.position.set(r * (width + 0.05), 0, c * (width + 0.05));
-          outline.position.set(r * (width + 0.05), 0, c * (width + 0.05));
-          outline.scale.multiplyScalar(1.05);
+            cube.position.set(r * (width + 0.05), 0, c * (width + 0.05));
+            outline.position.set(r * (width + 0.05), 0, c * (width + 0.05));
+            outline.scale.multiplyScalar(1.05);
 
-          shape.add(cube);
-          shape.add(outline);
+            shape.add(cube);
+            shape.add(outline);
 
-          board.push(cube);
-          outlines.push(outline);
-          newGroup.add(shape);
-      }
-  }
-  group = newGroup;
-  group.name = 'CUBE GROUP';
-  scene.add(group);
-  rows = newRows;
-  cols = rows;
+            board.push(cube);
+            outlines.push(outline);
+            newGroup.add(shape);
+        }
+    }
+    group = newGroup;
+    group.name = 'CUBE GROUP';
+    scene.add(group);
+    rows = newRows;
+    cols = rows;
 }
 
 
@@ -248,24 +275,26 @@ let node = {
     toggle: toggleNode,
     clear: clearAll,
     clearWall: clearWall,
-    rows: rows*2,
+    rows: rows * 2,
     diagonal: false
 }
 
 
 const visualizeDFS = (type) => {
-  getBoard();
-  dfs(board, boardCoor, boardPath, startCoor, targetCoor, node.diagonal, node.rows);
+    getBoard();
+    reset(boardPath, startCoor);
+    dfs(board, boardCoor, boardPath, startCoor, startCoor, targetCoor, node.diagonal, node.rows);
 }
 
 const visualizeBFS = (type) => {
-  getBoard();
-  bfs(board, boardCoor, boardPath, startCoor, targetCoor, node.diagonal, node.rows);
+    getBoard();
+    reset(boardPath, startCoor);
+    bfs(board, boardCoor, boardPath, startCoor, targetCoor, node.diagonal, node.rows);
 }
 
 let pathfinding = {
-  visualizeDFS: visualizeDFS,
-  visualizeBFS: visualizeBFS
+    visualizeDFS: visualizeDFS,
+    visualizeBFS: visualizeBFS
 }
 
 const gui = new dat.GUI();
@@ -286,16 +315,16 @@ options.add(node, "rows", 10, 40, 10);
 
 let previousRows = rows;
 
-const animate = function () {
-    requestAnimationFrame( animate );
+const animate = function() {
+    requestAnimationFrame(animate);
 
     if (node.rows !== previousRows) {
-      resizeBoard((node.rows/2));
+        resizeBoard((node.rows / 2));
     }
     previousRows = node.rows;
     controls.update();
     stats.begin();
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
     stats.end();
 
 
@@ -305,7 +334,10 @@ const animate = function () {
 animate();
 
 const windowResizeHanlder = () => {
-    const { innerHeight, innerWidth } = window;
+    const {
+        innerHeight,
+        innerWidth
+    } = window;
     renderer.setSize(innerWidth, innerHeight);
     camera.aspect = innerWidth / innerHeight;
     camera.updateProjectionMatrix();
@@ -344,7 +376,7 @@ const click = (cube, type) => {
     }
     let r = Math.round((2 * rows) * (rows + (cube.position.x / (width + 0.05))));
     let c = Math.round(cols + (cube.position.z / (width + 0.05)));
-    if( cube.material.color.getHex() === ground.material.color.getHex() || cubeType !== type ) {
+    if (cube.material.color.getHex() === ground.material.color.getHex() || cubeType !== type) {
         if (type === "Start Node") {
             clearType(start);
             cube.material = start.material;
@@ -352,7 +384,7 @@ const click = (cube, type) => {
             cube.position.y = 0;
             outlines[r + c].scale.y = 1.05;
             outlines[r + c].position.y = 0;
-//            toggleNode();
+            //            toggleNode();
         } else if (type === "Target Node") {
             clearType(target);
             cube.material = target.material;
@@ -360,7 +392,7 @@ const click = (cube, type) => {
             cube.position.y = 0;
             outlines[r + c].scale.y = 1.05;
             outlines[r + c].position.y = 0;
-//            toggleNode();
+            //            toggleNode();
         } else {
             if (type === "Wall Node") {
                 cube.material = build.material;
@@ -382,11 +414,11 @@ const click = (cube, type) => {
     }
     board[r + c] = cube;
 
-//    if (cubeType === "Start Node") {
-//        cs.textContent = cubeType;
-//    } else if (cubeType === "Target Node") {
-//        cs.textContent = cubeType;
-//    }
+    //    if (cubeType === "Start Node") {
+    //        cs.textContent = cubeType;
+    //    } else if (cubeType === "Target Node") {
+    //        cs.textContent = cubeType;
+    //    }
 
 }
 
@@ -396,18 +428,18 @@ let raycaster = new THREE.Raycaster();
 let mouse = new THREE.Vector2();
 
 const toggleWall = (event) => {
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-    raycaster.setFromCamera( mouse, camera );
+    raycaster.setFromCamera(mouse, camera);
 
-	let intersects = raycaster.intersectObjects( board );
+    let intersects = raycaster.intersectObjects(board);
 
-	if ( (intersects.length > 0 && wall !== intersects[0].object) ) {
-	    click(intersects[0].object, cs.textContent);
-	}
+    if ((intersects.length > 0 && wall !== intersects[0].object)) {
+        click(intersects[0].object, cs.textContent);
+    }
 
-  return intersects[0].object;
+    return intersects[0].object;
 
 }
 
@@ -415,21 +447,20 @@ let wall = null;
 let mouseDown = 0;
 
 document.body.onmousedown = function(event) {
-    if(event.button === 0) {
+    if (event.button === 0) {
         ++mouseDown;
         wall = toggleWall(event);
     }
 }
 document.body.onmouseup = function(event) {
-    if(event.button === 0) {
+    if (event.button === 0) {
         --mouseDown;
         wall = null;
     }
 }
 
-document.addEventListener( 'mousemove', function( event ) {
+document.addEventListener('mousemove', function(event) {
     if (mouseDown > 0) {
         wall = toggleWall(event);
     }
-}
-, false );
+}, false);
